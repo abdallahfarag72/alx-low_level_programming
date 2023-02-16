8293c5d3b3dfdcb667f6de19910fd68fe5a45178
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-16 15:12:35 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * main - The start of the program
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
