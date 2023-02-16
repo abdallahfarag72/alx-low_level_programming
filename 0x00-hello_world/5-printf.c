382cd33db60853273404c4e3d8be328a212939fd
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-16 15:32:35 -0800

    hello puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+   * main - The start of the program
+   * Return: The sum of the two parameters
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
