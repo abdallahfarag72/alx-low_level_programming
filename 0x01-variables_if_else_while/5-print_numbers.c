1f7793c35b530d1c78100de76277664089536c9d
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-17 13:00:55 -0800

    c basics numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The start of the program
+ * Return: The integer 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
