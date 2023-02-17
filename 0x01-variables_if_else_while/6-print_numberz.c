fb0fb342dbacc122eff3306a3f3f2b312a7addfe
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-17 13:30:03 -0800

    c basics char numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The start of the program
+ * Return: The integer 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
