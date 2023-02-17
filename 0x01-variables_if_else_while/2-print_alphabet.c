2f082629a14524e6b24997f48d262325a045c256
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-17 12:31:30 -0800

    c basics alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The start of the program
+ * Return: The integer 0
+*/
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x = x + 1;
+	}
+}
