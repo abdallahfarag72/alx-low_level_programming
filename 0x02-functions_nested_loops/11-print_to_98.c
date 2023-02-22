547fe725d9b3510d8a84d75790b67c8b81bdbc35
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-22 11:29:11 -0800

    function print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,40 @@
 */
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	while (n != 98)
 	{
+		int i;
+
+		i = n;
+		if (i < 0)
+		{
+			_putchar('-');
+			i = i * -1;
+		}
 		if (i < 10)
 		{
 			_putchar(i + '0');
 		}
 		else
 		{
-			_putchar(i / 10 + '0');
+			if (i > 99)
+			{
+				_putchar(i / 100 + '0');
+			}
+			_putchar((i / 10) % 10 + '0');
 			_putchar(i % 10 + '0');
 		}
-		if (i != 98)
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			n--;
+		}
+		else
+		{
+			n++;
 		}
 	}
+	_putchar('9');
+	_putchar('8');
 }
