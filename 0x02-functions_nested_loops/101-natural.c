d622f178a36d8f9153fc16aeebbf4a90137fb149
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-22 13:50:24 -0800

    function multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_mutltiples - prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: void
+*/
+void print_mutltiples(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	_putchar(sum / 100000 + '0');
+	_putchar((sum / 10000) % 10 + '0');
+	_putchar((sum / 1000) % 10 + '0');
+	_putchar((sum / 100) % 10 + '0');
+	_putchar((sum / 10) % 10 + '0');
+	_putchar(sum % 10 + '0');
+}
