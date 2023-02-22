b68aae8e45235b04826db918985ee6ed52683b82
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-21 18:55:56 -0800

    function jack_bauer minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints all minutes of the day.
+ * Return: void
+*/
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
