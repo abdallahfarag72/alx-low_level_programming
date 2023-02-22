e6c883ea478b9f72ea8cf90d4fd65a2cd7d930fa
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-02-22 13:18:29 -0800

    function n-times-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,33 @@ void print_times_table(int n)
 	int i, j, res;
 
 	if (n > 15 || n < 0)
-	{
 		return;
-	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			res = i * j;
-			_putchar(' ');
-			if (res <= 99)
-			{
-				_putchar(' ');
-			}
 			if (res < 10)
 			{
-				if (j != 0)
+				if (j > 0)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(' ');
 				}
 				_putchar(res + '0');
 			}
+			else if (res <= 99)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((res / 10) % 10 + '0');
+				_putchar(res % 10 + '0');
+			}
 			else
 			{
-				if (res > 99)
-				{
-					_putchar(res / 100 + '0');
-				}
+				_putchar(' ');
+				_putchar(res / 100 + '0');
 				_putchar((res / 10) % 10 + '0');
 				_putchar(res % 10 + '0');
 			}
