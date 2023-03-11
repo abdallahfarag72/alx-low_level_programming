afa12678444df80031e60955a41b2f4c603bcd7a
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-10 16:01:40 -0800

    argc number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	_putchar(argc - 1 + '0');
+	_putchar('\n');
+	return (0);
+}
