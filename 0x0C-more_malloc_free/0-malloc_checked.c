3731a6f0165e3dcaef8ccfbfbf8bf540e7443643
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-16 15:24:15 -0700

    malloc exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: bytes of allocated memory
+ * Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		return (NULL);
+		exit(98);
+	}
+	return (p);
+	free(p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+## 0x0C-more_malloc_free
+This directory contains the tasks for the malloc/free project.
