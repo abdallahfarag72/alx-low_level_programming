b8a566d19f309d1654f84ffcf4e794544d17e350
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-20 18:50:02 -0700

    function free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
