a61fa7761c13b9f24998e1f54c830263e596670f
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-20 18:33:42 -0700

    function print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->age)
-	{
-		printf("Age: %f\n", d->age);
-	}
-	else
-	{
-		printf("Age: (nil)\n");
-	}
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
