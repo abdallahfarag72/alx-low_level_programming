6d5c08d3442298f9440590dcd06aa07a5abb3b72
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-20 17:34:53 -0700

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+## 0x0E-structures_typedef
+This directory contains the tasks for the structures project.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - gives info about a dog
+ * @name: name of dog as string
+ * @age: age of dog as float
+ * @owner: name of owner as string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG*/
