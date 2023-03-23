c01675b6f2fc28fb2b709516af5be99317085921
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-22 17:54:51 -0700

    function pointers search index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -17,10 +16,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 	{
 		return (-1);
-	}
-	if (array == NULL || cmp == NULL)
-	{
-		exit(0);
 	}
 
 	for (i = 0; i < size; i++)
