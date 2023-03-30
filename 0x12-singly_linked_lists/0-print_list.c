dab7f66605c05669b553917ac4e63572ec3a056e
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-03-30 09:12:41 -0700

    sll print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to list to be printed
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *node;
+	size_t node_count;
+
+	node = malloc(sizeof(list_t));
+	node = h;
+	node_count = 0;
+	while (node != NULL)
+	{
+		if (node->str)
+		{
+			printf("[%d] %s\n", node->len, node->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		node_count++;
+		node = node->next;
+	}
+	return (node_count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+## 0x12-singly_linked_lists
+This directory has the tasks for the SLL project.
