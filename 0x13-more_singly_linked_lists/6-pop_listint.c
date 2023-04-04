6dde294a47650c25a9953966476115ac3706b125
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-04-04 15:10:43 -0700

    sll get_nodeint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,7 +14,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to head
+ * @index: index of node starting at 0
+ *
+ * Return: node if it exists and null otherwise
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		head = head->next;
+	}
+	return (head);
+}
