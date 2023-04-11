c896941d6417a5210431dbad1c285a647c1f8bc0
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-04-10 17:47:35 -0700

    file-io append_to_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: file to be read
+ * @text_content: string to appended to the file
+ *
+ * Return: actual number of letters it could read and print
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, length;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (text_content[length])
+			length++;
+		bytes_written = write(fd, text_content, length);
+		if (bytes_written == -1 || bytes_written != length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
