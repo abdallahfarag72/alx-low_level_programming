dbe4dd7ecbc38ba5d69b46dd5c7fbea0784bde15
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-18 16:55:16 -0700

    DLL length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,20 +3,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
+ * dlistint_len - returns the number of elements
+ * in a linked dlistint_t list.
  * @h: pointer to list
  *
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count;
 
 	count = 0;
 	while (h)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
