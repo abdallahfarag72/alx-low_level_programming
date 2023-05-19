92bd2ab3c585a20abf1d5dbdfbc35c88ad8d6fff
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-18 17:17:16 -0700

    DLL add_to_beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the
+ * beginning of a dlistint_t list.
+ * @head: pointer to list
+ * @n: data to be added
+ *
+ * Return: the number of nodes
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
+}
