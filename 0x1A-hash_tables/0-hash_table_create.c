059607c47ea0db7815acbb971344884f3cdc917d
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-09 14:56:52 -0700

    djb2 hash function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->size = size;
-	new_ht->array = calloc(new_ht->size, sizeof(hash_node_t *));
+	new_ht->array = malloc(new_ht->size *  sizeof(hash_node_t *));
 	if (new_ht->array == NULL)
 	{
 		free(new_ht);
