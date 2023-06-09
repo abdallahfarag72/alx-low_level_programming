9a7fae184e74f6ffc9a26302234a32b8bd55a244
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-09 15:48:13 -0700

    key_index hash function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,8 @@
 /**
-*hash_djb2 - implementation of the djb2 algorithm
+* hash_djb2 - implementation of the djb2 algorithm
 * @str: string used to generate hash value
 *
-*Return : hash value
+* Return: hash value
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
