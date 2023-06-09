a43505d849a2746670ebb97e9619a7b65c326b23
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-09 14:35:46 -0700

    create hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the new hash table
+ *
+ * Return: a pointer to the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht;
+	unsigned long int i;
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+	new_ht->size = size;
+	new_ht->array = calloc(new_ht->size, sizeof(hash_node_t *));
+	if (new_ht->array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < new_ht->size; i++)
+		new_ht->array[i] = NULL;
+
+	return (new_ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+# 0x1A-hash_tables
+This directory contains the tasks for the hash tables project.
